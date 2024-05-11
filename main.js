231a8e1450b26e079d8066a7d4a5c849882e15aa
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-05-11 00:48:45 -0500

    adding some code to improve ux

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: 'Socks',
-      description: 'Pair of feet socks, for when it´s cold',
+      description: 'When it´s cold',
+      image: './assets/images/socks_green.jpg',
+      url: 'https://www.vuemastery.com/',
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+      ],
+
     }
-  }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+
+    updateImage(variantImage) {
+      this.image = variantImage;
+    }
+  },
 })
