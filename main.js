70f19b914b4a9606563138c8122ac2fa8b7b9ee8
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-05-11 15:38:52 -0500

    using computed properties to show data into html

--- main.js
+++ main.js
@@ -3,14 +3,23 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: 'Socks',
+      brand: 'Vue Mastery',
       description: 'When it´s cold',
-      image: './assets/images/socks_green.jpg',
+      selectedVariant: 0,
       url: 'https://www.vuemastery.com/',
-      inStock: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+        { 
+          id: 2234, 
+          color: 'green', 
+          image: './assets/images/socks_green.jpg',
+          quantity: 50,
+        },
+        { id: 2235, 
+          color: 'blue', 
+          image: './assets/images/socks_blue.jpg',
+          quantity: 0,
+        },
       ],
 
     }
@@ -20,8 +29,22 @@ const app = Vue.createApp({
       this.cart += 1;
     },
 
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
     }
   },
+
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+
+    inStock() {
+      return this.variants[this.selectedVariant].quantity
+    }
+  }
 })
