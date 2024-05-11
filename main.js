1f79ba3623b94887771e64f7262bfe7a2a6a8a63
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-05-11 00:12:52 -0500

    initializing vue app with options API

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks';
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      description: 'Pair of feet socks, for when it´s cold',
+    }
+  }
+})
